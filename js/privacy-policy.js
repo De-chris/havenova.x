e804e1e8c5dd2b600c37c0177e496d48d6c7b49c
justanoobofficial <justanoobofficial@github.com>
2026-01-07 21:29:51 +0100

    Edited by HAVENOVA-X Team

--- js/privacy-policy.js
+++
@@ -1,7 +0,0 @@
-const toggle = document.getElementById('darkModeToggle');
-toggle.onclick = () => {
-  document.body.classList.toggle('dark');
-  const icon = toggle.querySelector('i');
-  icon.classList.toggle('fa-moon');
-  icon.classList.toggle('fa-sun');
-};
